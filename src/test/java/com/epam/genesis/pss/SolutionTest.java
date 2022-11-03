39c19761d5d01856a21b54ef3db0ca08c0055ebb
Venu_kandagatla <venu_kandagatla@epam.com>
2022-11-03 18:23:31 +0530

    so
    ssfd

---
+++ src/test/java/com/epam/genesis/pss/SolutionTest.java
@@ -0,0 +1,13 @@
+package com.epam.genesis.pss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void topKFrequentWords() {
+        assertTrue(true);
+    }
+}
